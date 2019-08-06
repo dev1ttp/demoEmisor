9c771762be332c0f92a1908250a9d00963976b1c
dev1ttp <developer1@totalpack.cl>
2019-08-06 13:02:11 -0400

    cambios demo

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, isDevMode } from '@angular/core';
 import { Demo } from 'src/app/interfaces/demo';
 import { Demo2 } from 'src/app/interfaces/demo2';
 import { DemoService } from 'src/app/services/demo.service';
+import { DOCUMENT } from '@angular/common';
+
 
 @Component({
   selector: 'app-demo',
@@ -10,17 +12,31 @@ import { DemoService } from 'src/app/services/demo.service';
 })
 export class DemoComponent implements OnInit {
 
+  //styleUrls: ['./demo.component.css']
+
   public d1: Array<Demo>;
   public d2: Array<Demo2>;
 
+  public current: any;
+
   constructor(
-    private _demo: DemoService
+    private _demo: DemoService,
+
   ) { }
 
   ngOnInit() {
 
     this.getInfoFromDemo();
     this.getInfoFromDemo2();
+
+    this.current = "styles1.css";
+
+
+      //require("style-loader!./../../../assets/css/styles1.css");
+      //var asd = require("style-loader!./../../../assets/css/"+this.current)
+
+      //require("style-loader!./../../../"+this.current);
+
   }
 
 
@@ -37,5 +53,17 @@ export class DemoComponent implements OnInit {
     this.d2 = response['data'];
     console.log(this.d2);
   }
+
+  public async updateStyle(fileName: string){
+
+    
+    require("style-loader!./../../../assets/css/"+fileName);
+    console.log(fileName);
+
+    //console.log(document.styleSheets)
+
+  }
+
+
 
 }
