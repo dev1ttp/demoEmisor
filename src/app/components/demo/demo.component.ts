62c768212e1da89bddbd74a9d6caaa870ebcc2c1
dev1ttp <developer1@totalpack.cl>
2019-08-07 16:43:08 -0400

    cambios democomponent

--- src/app/components/demo/demo.component.ts
+++ src/app/components/demo/demo.component.ts
@@ -11,8 +11,6 @@ import * as $ from 'jquery';
 })
 export class DemoComponent implements OnInit {
 
-  //styleUrls: ['./demo.component.css']
-
   public d1: Array<Demo>;
   public d2: Array<Demo2>;
 
@@ -25,50 +23,68 @@ export class DemoComponent implements OnInit {
 
   ngOnInit() {
 
-    this.getInfoFromDemo();
-    this.getInfoFromDemo2();
+    this.getCSSFromDemo();
+    this.getJSFromDemo2();
 
-    this.current = "styles1.css";
-
-
-      //require("style-loader!./../../../assets/css/styles1.css");
-      //var asd = require("style-loader!./../../../assets/css/"+this.current)
-
-      //console.log(document.styleSheets)
-
-
-
-      //require("style-loader!./../../../"+this.current);
+    //this.current = "styles1.css";
+    //require("style-loader!./../../../assets/css/styles1.css");
+    //var asd = require("style-loader!./../../../assets/css/"+this.current)
+    //console.log(document.styleSheets)
+    //require("style-loader!./../../../"+this.current);
 
   }
 
 
-  public async getInfoFromDemo(){
+  public async getCSSFromDemo() {
 
     let response = await this._demo.getData();
     this.d1 = response['data'];
     console.log(this.d1);
   }
 
-  public async getInfoFromDemo2(){
+  public async getJSFromDemo2() {
 
     let response = await this._demo.getData2();
     this.d2 = response['data'];
     console.log(this.d2);
   }
 
-  public async updateStyle(fileName: string){
+  public async loadStyle(fileName: string) {
 
-/*     $(document).ready(function(){
+    let head = document.getElementsByTagName('head')[0];
+    //head.removeChild();
 
-      $("link[href='styles1.css']").remove();
+    /* console.log(document.getElementsByTagName('head')[0].childNodes[14].remove);
+    console.log(document.getElementsByTagName('head')[0].childNodes); */
+    
 
-    }); */
+    //let childLenght = document.getElementsByTagName('head')[0].childNodes.length;
+    //let childRemove = document.getElementsByTagName('head')[0].getElementsByTagName('script')[childLenght-1];
+
+    document.getElementsByTagName('head')[0].parentNode;
+
+    
+
+
+
+
+    //console.log(document.getElementsByTagName('head')[0].childNodes[childLenght - 1]);
+
+    
+
+    /*     $(document).ready(function(){
+    
+          $("link[href='styles1.css']").remove();
+    
+        }); */
 
     //document.styleSheets[document.styleSheets.length - 1].disabled = true;
-    
-    require("style-loader!./../../../assets/css/"+fileName);
 
+    //location.reload(true);
+
+    require("style-loader!./../../../assets/css/" + fileName);
+
+    //location.reload(true);
 
     console.log(fileName);
 
